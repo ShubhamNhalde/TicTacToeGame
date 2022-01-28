01c4ff450b9779d2f897baa216e8fe146b9a88b7
Shubham <nhaldes@gmail.com>
2022-01-28 11:44:09 +0530

    Added: Board

---
+++ GameBoard.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.TicTacToeGame;
+
+public class GameBoard {
+	
+	public static void startboard() {
+		char[] board = { ' ', ' ',' ',' ',' ',' ',' ',' ',' ',' ',};
+	}
+
+	public static void main(String[] args){
+		System.out.println("Welcom to TicTacToe games");
+		startboard();
+	}
+
+}
